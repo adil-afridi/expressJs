d3109508bc78b1dbd6ea686a7fba49f2891c83f1
adil afridi <adiljamal002@gmail.com>
2024-09-18 16:19:06 +0500

    post add method has been declear

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
-import express from 'express';
-
+import express from "express";
 
 const app = express();
 const Port = 3000;
@@ -7,93 +6,100 @@ const Port = 3000;
 app.use(epxress.json());
 
 const items = [
-    {
-        id:1,
-        name: 'Jamal Khan',
-        age: 22,
-        city: 'Peshawar'
-    },
-    {
-        id:2,
-        name: 'Kamal Khan',
-        age: 30,
-        city: 'Islamabad'
-    },
-    {
-        id:3,
-        name: 'Amir Khan',
-        age: 227,
-        city: 'Karachi'
-    },
-    {
-        id:4,
-        name: 'Kabeer Khan',
-        age: 35,
-        city: 'Lahore'
-    },
-    {
-        id:5,
-        name: 'AJ Afridi',
-        age: 24,
-        city: 'Peshawar'
-    }
-]
+  {
+    id: 1,
+    name: "Jamal Khan",
+    age: 22,
+    city: "Peshawar",
+  },
+  {
+    id: 2,
+    name: "Kamal Khan",
+    age: 30,
+    city: "Islamabad",
+  },
+  {
+    id: 3,
+    name: "Amir Khan",
+    age: 227,
+    city: "Karachi",
+  },
+  {
+    id: 4,
+    name: "Kabeer Khan",
+    age: 35,
+    city: "Lahore",
+  },
+  {
+    id: 5,
+    name: "AJ Afridi",
+    age: 24,
+    city: "Peshawar",
+  },
+];
 
+///Root of Home Page
+app.get("/", (req, res) => {
+  res.send("Wellcome to Home Page");
+});
 
-//Root of Home Page
-app.get('/',(req , res)=>{
-    res.send('Wellcome to Home Page')
-})
+/// Root of About Page
+app.get("/about", (req, res) => {
+  res.send("Wellcome to About Page");
+});
 
+///Post request to about
+app.post("/about", (req, res) => {
+  const data = req.body;
+  res.send(`Data Recived on About Page ${JSON.stringify(data)}`);
+});
 
-// Root of About Page
-app.get('/about', (req , res)=>{
-    res.send('Wellcome to About Page')
-})
+///PUT request to about
+app.put("/about", (req, res) => {
+  const updateDate = req.body;
+  res.send(`Data is updated on About Page ${JSON.stringify(updateDate)}`);
+});
 
+/// DELETE request to about
+app.delete("/about", (req, res) => {
+  res.send("Succesfully Deleted data About Page..");
+});
 
-//Post request to about 
-app.post('/about',(req,res)=>{
-    const data = req.body;
-    res.send(`Data Recived on About Page ${JSON.stringify(data)}`)
-})
+/// Contact Page
+app.get("/contact", (req, res) => {
+  res.send("Wellcome to Contact Page");
+});
 
+///Post request to contact
+app.post("/contact", (req, res) => {
+  const data = req.body;
+  res.send(`Data Recived on Contact Page ${JSON.stringify(data)}`);
+});
 
-//PUT request to about 
-app.put('/about',(req,res)=>{
-    const updateDate = req.body;
-    res.send(`Data is updated on About Page ${JSON.stringify(updateDate)}`)
-})
+///PUT request to Contact
+app.put("/about", (req, res) => {
+  const updateDate = req.body;
+  res.send(`Data is updated on Contact Page ${JSON.stringify(updateDate)}`);
+});
 
-// DELETE request to about
+/// DELETE request to Contact
+app.delete("/about", (req, res) => {
+  res.send("Succesfully Deleted data from Contact Page..");
+});
 
-app.delete('/about',(req,res)=>{
-    res.send('Succesfully Deleted data About Page..')
-})
+// get Retrive all items now
+app.get("/items", (req, res) => {
+  res.status(200).json(items);
+});
 
-
-// Contact Page
-
-app.get('/contact',(req,res)=>{
-    res.send('Wellcome to Contact Page')
-})
-
-
-//Post request to contact 
-app.post('/contact',(req,res)=>{
-    const data = req.body;
-    res.send(`Data Recived on Contact Page ${JSON.stringify(data)}`)
-})
-
-
-//PUT request to Contact 
-app.put('/about',(req,res)=>{
-    const updateDate = req.body;
-    res.send(`Data is updated on Contact Page ${JSON.stringify(updateDate)}`)
-})
-
-// DELETE request to Contact
-
-app.delete('/about',(req,res)=>{
-    res.send('Succesfully Deleted data from Contact Page..')
-})
+/// post add a new items
+app.post("/items", (req, res) => {
+  const newItem = {
+    id: items.length + 1,
+    name: req.body.name,
+    age: req.body.age,
+    city: req.body.city,
+  };
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
