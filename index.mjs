5497306d8ba92ddad017ac6e2a51d10eb0535f20
adil afridi <adiljamal002@gmail.com>
2024-09-18 15:54:16 +0500

    now all arry has declear

--- index.mjs
+++ index.mjs
@@ -6,4 +6,35 @@ const Port = 3000;
 
 app.use(epxress.json());
 
-
+const items = [
+    {
+        id:1,
+        name: 'Jamal Khan',
+        age: 22,
+        city: 'Peshawar'
+    },
+    {
+        id:2,
+        name: 'Kamal Khan',
+        age: 30,
+        city: 'Islamabad'
+    },
+    {
+        id:3,
+        name: 'Amir Khan',
+        age: 227,
+        city: 'Karachi'
+    },
+    {
+        id:4,
+        name: 'Kabeer Khan',
+        age: 35,
+        city: 'Lahore'
+    },
+    {
+        id:5,
+        name: 'AJ Afridi',
+        age: 24,
+        city: 'Peshawar'
+    }
+]
